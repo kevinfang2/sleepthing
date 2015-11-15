6f0a393ad77768061cbd38ee82d44e9252dfc029
kevinfang2 <kevkev0123@gmail.com>
2015-11-14 20:46:43 -0800

    asdf

--- pillowHack/pillowHack/ViewController.m
+++ pillowHack/pillowHack/ViewController.m
@@ -29,44 +29,73 @@
 @implementation ViewController {
     
     __weak IBOutlet UIButton *start;
+    __weak IBOutlet UIButton *end;
+}
+int a;
+- (IBAction)start:(id)sender {
+//    [self gyroscope];
+    start.hidden = YES;
+    
+}
+- (IBAction)end:(id)sender {
+    NSLog(@"asdf");
+    start.hidden= NO;
 }
 
-- (IBAction)start:(id)sender {
-    yTotal = 0;
-    [super viewDidLoad];
-    NSLog(@"asdf");
-    UILabel* yLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, _width, 50)];
-    [self.view addSubview:yLabel];
-    yLabel.textAlignment = NSTextAlignmentCenter;
-    
-    UILabel* yLabelChange = [[UILabel alloc] initWithFrame:CGRectMake(10, 60, _width, 50)];
-    [self.view addSubview:yLabelChange];
-    yLabelChange.textAlignment = NSTextAlignmentCenter;
-    
-    motionManager = [[CMMotionManager alloc] init];
-    if([motionManager isGyroAvailable])
-    {
-        if([motionManager isGyroActive] == NO)
+
+//- (void)timer{
+//    NSTimer *timer = [[timer alloc] init];
+//    
+//    [timer startTimer];
+//    // Do some work
+//    [timer stopTimer];
+//    
+//    NSLog(@"Total time was: %lf milliseconds", [timer timeElapsedInMilliseconds]);
+//    NSLog(@"Total time was: %lf seconds", [timer timeElapsedInSeconds]);
+//    NSLog(@"Total time was: %lf minutes", [timer timeElapsedInMinutes]);
+//    
+//}
+- (void)gyroscope{
+    if(start.hidden == NO){
+        yTotal = 0;
+        [super viewDidLoad];
+        NSLog(@"asdf");
+        UILabel* yLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, _width, 50)];
+        [self.view addSubview:yLabel];
+        yLabel.textAlignment = NSTextAlignmentCenter;
+        
+        UILabel* yLabelChange = [[UILabel alloc] initWithFrame:CGRectMake(10, 60, _width, 50)];
+        [self.view addSubview:yLabelChange];
+        yLabelChange.textAlignment = NSTextAlignmentCenter;
+        
+        motionManager = [[CMMotionManager alloc] init];
+        if([motionManager isGyroAvailable])
         {
-            [motionManager setGyroUpdateInterval:1.0f / 10.0f];
-            [motionManager startGyroUpdatesToQueue:[NSOperationQueue mainQueue]
-                                       withHandler:^(CMGyroData *gyroData, NSError *error)
-             {
-                 float degs = RADIANS_TO_DEGREES(gyroData.rotationRate.y) / 10;
-                 degs = degs - 0.4;
-                 if(degs > 0.2 || degs < -0.2)
+            if([motionManager isGyroActive] == NO)
+            {
+                [motionManager setGyroUpdateInterval:1.0f / 10.0f];
+                [motionManager startGyroUpdatesToQueue:[NSOperationQueue mainQueue]
+                                           withHandler:^(CMGyroData *gyroData, NSError *error)
                  {
-                     yTotal = yTotal + degs;
-                 }
-                 NSString *y = [[NSString alloc] initWithFormat:@"%.02f",yTotal];
-                 angle = yTotal;
-                 yLabel.text = y;
-                 yLabelChange.text = [[NSString alloc] initWithFormat:@"%.02f",degs];
-                 //                 [myRootRef setValue:[NSNumber numberWithFloat:yTotal]];
-             }];
+                     float degs = RADIANS_TO_DEGREES(gyroData.rotationRate.y) / 10;
+                     degs = degs - 0.4;
+                     if(degs > 0.2 || degs < -0.2)
+                     {
+                         yTotal = yTotal + degs;
+                     }
+                     NSString *y = [[NSString alloc] initWithFormat:@"%.02f",yTotal];
+                     angle = yTotal;
+                     yLabel.text = y;
+                     yLabelChange.text = [[NSString alloc] initWithFormat:@"%.02f",degs];
+                     //                 [myRootRef setValue:[NSNumber numberWithFloat:yTotal]];
+                 }];
+            }
+            //        [self sleeptimer];
         }
-        //        [self sleeptimer];
     }
+}
+
+- (AVCaptureDevice *)frontCamera {
     AVCaptureSession *session = [[AVCaptureSession alloc] init];
     session.sessionPreset = AVCaptureSessionPresetHigh;
     AVCaptureDevice *device = [self frontCamera];
@@ -79,11 +108,7 @@
     //    newCaptureVideoPreviewLayer.la
     [self.view.layer addSublayer:newCaptureVideoPreviewLayer];
     [session startRunning];
-}
 
-
-
-- (AVCaptureDevice *)frontCamera {
     NSArray *devices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
     for (AVCaptureDevice *device in devices) {
         if ([device position] == AVCaptureDevicePositionFront) {
